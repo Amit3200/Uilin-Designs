43293f3185133c66b559d1eb018796bfddff4bb1
Debasish Bhol <31538473+debasish-bhol@users.noreply.github.com>
2018-10-27 12:59:57 +0530

    Update footer.php

--- login/footer.php
+++ login/footer.php
@@ -1,5 +1,6 @@
 <html>
-    <title>Uilin Designs</title>
+    <title>Uilin-Designs</title>
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
